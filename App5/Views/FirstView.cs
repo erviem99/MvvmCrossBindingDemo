9e0ff57e49cdf6b1fe6028e7ce7a5215e92e559b
erik_f_andersen <eandersen9@gmail.com>
2016-04-11 13:26:31 +0200

    Add project files.

---
+++ App5/Views/FirstView.cs
@@ -0,0 +1,51 @@
+﻿using ClassLibrary.ViewModels;
+using CrossUI.iOS.Dialog.Elements;
+using MvvmCross.Binding.BindingContext;
+using MvvmCross.Dialog.iOS;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UIKit;
+
+namespace App5.Views
+{
+    public class FirstView: MvxDialogViewController
+    {
+        private EntryElement input;
+        private StringElement output;
+
+        public FirstView(): base(pushing: false)
+        {
+
+        }
+
+        public override void ViewDidLoad()
+        {
+            base.ViewDidLoad();
+
+            Root = new RootElement("Test")
+            {
+                new Section()
+                {
+                    (input = new EntryElement("Input")),
+                    (output = new StringElement())
+                }
+            };
+
+            var btn = new UIBarButtonItem(UIBarButtonSystemItem.Edit);
+            NavigationItem.RightBarButtonItem = btn;
+
+            var set = this.CreateBindingSet<FirstView, FirstViewModel>();
+            set.Bind(input).To(vm => vm.Hello);
+            set.Bind(output).To(vm => vm.Hello);
+            set.Apply();
+
+            btn.Clicked += Btn_Clicked;
+        }
+
+        private void Btn_Clicked(object sender, EventArgs e)
+        {
+            input.Value = "Click";
+        }
+    }
+}
